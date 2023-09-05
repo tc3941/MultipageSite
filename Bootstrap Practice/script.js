3d10093d287d7d9b1d2a7d3490002a88993d81bd
Teddy Conyers Jr <tc3941@rit.edu>
2023-09-04 20:43:05 -0400

    Add files via upload

---
+++ Bootstrap Practice/script.js
@@ -0,0 +1,20 @@
+let cups = 0;
+
+if (cups < 3) {
+  console.log("Yes I'll take another cup of coffee.");
+} else {
+  console.log("I think I'm okay for now.");
+}
+
+let miles = 499;
+
+if (miles > 500) {
+  console.log('Yes, I think I need an oil change.');
+} else {
+  console.log("My car doesn't need an oil change yet.");
+}
+
+for (let i = 0; i <= 99; i++) {
+  if (i % 2 === 0) console.log(i + ' is even');
+  else console.log(i + ' is odd');
+}
